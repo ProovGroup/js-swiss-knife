6a4ab8da65e4e302337e0682fee02a593c5fb86f
Damien <damienroche@free.fr>
2019-06-03 22:27:53 +0200

    isMobile fn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const removeElement = (el) => {
+export const removeElement = (el) => {
   if (typeof el.remove !== 'undefined') {
     el.remove()
   } else if (typeof el.parentNode !== 'undefined') {
@@ -6,4 +6,48 @@ const removeElement = (el) => {
   }
 }
 
-export removeElement
+/**
+ * Mobile detection
+ * https://www.abeautifulsite.net/detecting-mobile-devices-with-javascript
+ */
+export const isMobile = {
+  Android: function () {
+    return (
+      typeof window !== 'undefined' &&
+      window.navigator.userAgent.match(/Android/i)
+    )
+  },
+  BlackBerry: function () {
+    return (
+      typeof window !== 'undefined' &&
+      window.navigator.userAgent.match(/BlackBerry/i)
+    )
+  },
+  iOS: function () {
+    return (
+      typeof window !== 'undefined' &&
+      window.navigator.userAgent.match(/iPhone|iPad|iPod/i)
+    )
+  },
+  Opera: function () {
+    return (
+      typeof window !== 'undefined' &&
+      window.navigator.userAgent.match(/Opera Mini/i)
+    )
+  },
+  Windows: function () {
+    return (
+      typeof window !== 'undefined' &&
+      window.navigator.userAgent.match(/IEMobile/i)
+    )
+  },
+  any: function () {
+    return (
+      isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.iOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows()
+    )
+  }
+}
